6180cf024dfbe29fb4902465acacb0e438249f48
finkbeiner <nicofinkbeiner@gmail.com>
2023-06-24 09:58:56 +0200

    completed FRAM_write and FRAM_read

--- Firmware/Sailwind/FRAM/FRAM.c
+++ Firmware/Sailwind/FRAM/FRAM.c
@@ -25,11 +25,11 @@
 #define STATUS_REGISTER_BUFFER_SIZE 2U
 
 static uint8_t FRAM_read_status_register(void);
-static uint8_t FRAM_write_enable(void);
 
 
 uint8_t FRAM_write(uint8_t *pStructToSave, const uint32_t startAddress, uint32_t sizeInByte)
 {
+	HAL_SPI_StateTypeDef spiStatus;
 	uint8_t registerStatus;
 	uint32_t address;
 
@@ -47,10 +47,87 @@ uint8_t FRAM_write(uint8_t *pStructToSave, const uint32_t startAddress, uint32_t
 	address = little_to_big_endian(startAddress);
 
 	HAL_GPIO_WritePin(SPI4_CS_GPIO_Port, SPI4_CS_Pin, GPIO_PIN_RESET);
+	spiStatus = HAL_SPI_Transmit(&hspi4, WRITE, 1U, SPI_HAL_TIMEOUT);
+	if(spiStatus != HAL_OK)
+	{
+		printf("Failed sending Write instruction!\r\n");
+		HAL_GPIO_WritePin(SPI4_CS_GPIO_Port, SPI4_CS_Pin, GPIO_PIN_SET);
+		return FRAM_ERROR;
+	}
 
+	spiStatus = HAL_SPI_Transmit(&hspi4, (uint8_t*)&address, sizeof(address), SPI_HAL_TIMEOUT);
+	if(spiStatus != HAL_OK)
+	{
+		printf("Failed sending address!\r\n");
+		HAL_GPIO_WritePin(SPI4_CS_GPIO_Port, SPI4_CS_Pin, GPIO_PIN_SET);
+		return FRAM_ERROR;
+	}
 
+	hal_SPI_status= HAL_SPI_Transmit(&hspi3, struct_to_save, size_in_bytes, SPI_HAL_TIMEOUT);
+	if (hal_SPI_status != HAL_OK)
+	{
+		printf("Failed sending data to be saved!\r\n");
+		HAL_GPIO_WritePin(SPI4_CS_GPIO_Port, SPI4_CS_Pin, GPIO_PIN_SET);
+		return FRAM_ERROR;
+	}
 
+	HAL_GPIO_WritePin(SPI4_CS_GPIO_Port, SPI4_CS_Pin, GPIO_PIN_SET);
 
+	return FRAM_OK;
+
+}
+
+uint8_t FRAM_read(uint32_t startAddress, uint8_t *pData, const uint16_t sizeInByte)
+{
+	HAL_SPI_StateTypeDef spiStatus;
+	uint8_t tx_dummy[sizeInByte];
+	uint32_t address;
+
+	assert(HAL_GPIO_ReadPin(SPI4_CS_GPIO_Port, SPI4_CS_Pin) != 0);
+	assert(pData != 0);
+	assert(sizeInByte != 0);
+
+	address = little_to_big_endian(startAddress);
+
+	HAL_GPIO_WritePin(SPI4_CS_GPIO_Port, SPI4_CS_Pin, GPIO_PIN_RESET);
+
+	spiSTatus = HAL_SPI_Transmit(&hspi4, READ, 1U, SPI_HAL_TIMEOUT);
+	if(spiStatus != HAL_OK)
+	{
+		printf("Failed sending READ!\r\n");
+		HAL_GPIO_WritePin(SPI4_CS_GPIO_Port, SPI4_CS_Pin, GPIO_PIN_SET);
+		return FRAM_ERROR;
+	}
+
+	spiStatus = HAL_SPI_Transmit(&hspi4, (uint8_t*)&address, sizeof(address), SPI_HAL_TIMEOUT);
+	if(spiStatus != HAL_OK)
+	{
+		printf("Failed sending address!\r\n");
+		HAL_GPIO_WritePin(SPI4_CS_GPIO_Port, SPI4_CS_Pin, GPIO_PIN_SET);
+		return FRAM_ERROR;
+	}
+
+	spiStatus = HAL_SPI_Transmit(&hspi4, tx_dummy[0], 1U, SPI_HAL_TIMEOUT);
+	if(spiStatus != HAL_OK)
+	{
+		printf("Failed sending dummy byte!\r\n");
+		HAL_GPIO_WritePin(SPI4_CS_GPIO_Port, SPI4_CS_Pin, GPIO_PIN_SET);
+		return FRAM_ERROR;
+	}
+
+	spiStatus = HAL_SPI_TransmitReceive(&hspi4, tx_dummy, pData, sizeInByte, SPI_HAL_TIMEOUT);
+	if(spiStatus != HAL_OK)
+	{
+		printf("Failed receiving data!\r\n");
+		HAL_GPIO_WritePin(SPI4_CS_GPIO_Port, SPI4_CS_Pin, GPIO_PIN_SET);
+		return FRAM_ERROR;
+	}
+
+	HAL_Delay(1);
+
+	HAL_GPIO_WritePin(SPI4_CS_GPIO_Port, SPI4_CS_Pin, GPIO_PIN_SET);
+
+	return FRAM_OK;
 }
 
 static uint8_t FRAM_write_enable(void)
@@ -95,7 +172,7 @@ uint8_t FRAM_init(void)
 	return FRAM_OK;
 }
 
-static uint8_t FRAM_read_status_register()
+uint8_t FRAM_read_status_register()
 {
 	HAL_SPI_StateTypeDef spiStatus;
 	uint8_t statusRegTx[STATUS_REGISTER_BUFFER_SIZE];
--- Firmware/Sailwind/FRAM/FRAM.h
+++ Firmware/Sailwind/FRAM/FRAM.h
@@ -11,5 +11,7 @@
 uint32_t little_to_big_endian(uint32_t address);
 uint8_t FRAM_init(void);
 uint8_t FRAM_write(uint8_t *pStructToSave, const uint32_t startAddress, uint32_t sizeInByte);
+uint8_t FRAM_read(uint32_t startAddress, uint8_t *pData, const uint16_t sizeInByte);
+uint8_t FRAM_write_enable(void);
 
 #endif /* FRAM_H_ */
