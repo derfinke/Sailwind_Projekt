db7f4e5e2e455568dde9ac9437c6e19a926254c7
finkbeiner <nicofinkbeiner@gmail.com>
2023-08-27 14:25:39 +0200

    added protection for input and improved command construction

--- Firmware/Sailwind/WSWD/WSWD.c
+++ Firmware/Sailwind/WSWD/WSWD.c
@@ -11,10 +11,12 @@
 #include "UART.h"
 #include "main.h"
 
-#define GET_SERIAL_NUMBER "0002\r\n"
-#define WSWD_ID           "00"
-#define SIZE_OF_WSWD_ANSWER 11U
-#define SIZE_OF_WSWD_COMMAND 6U
+#define GET_SERIAL_NUMBER     "02"
+#define WSWD_ID               "00"
+#define SIZE_OF_WSWD_ID       2U
+#define SIZE_OF_WSWD_ANSWER   11U
+#define SIZE_OF_WSWD_COMMAND  6U
+#define SIZE_OF_WSWD_PARAM    5U
 
 static void WSWD_enable_receive(void);
 static void WSWD_enable_send(void);
@@ -42,8 +44,33 @@ static void WSWD_enable_send(void) {
 uint8_t WSWD_send_without_param(char* command)
 {
   assert(command != 0);
+  if((sizeof(command) - 1) > SIZE_OF_WSWD_COMMAND)
+  {
+    return 1;
+    printf("WSWD command has invalid size\r\n");
+  }
   WSWD_enable_send();
+  char command_structure[SIZE_OF_WSWD_COMMAND];
+  snprintf(command_structure, SIZE_OF_WSWD_COMMAND, "%s%s\r\n", WSWD_ID, command);
   if(HAL_UART_Transmit(&huart2, (uint8_t*)command, SIZE_OF_WSWD_COMMAND, UART_TX_TIME_OUT) != HAL_OK)
+  {
+    printf("error sending to WSWD\r\n");
+  }
+  return HAL_OK;
+}
+
+uint8_t WSWD_send_with_param(char* command, char* param)
+{
+  assert(command != 0);
+  if((sizeof(command) - 1) > SIZE_OF_WSWD_COMMAND || (sizeof(param) - 1) > SIZE_OF_WSWD_PARAM)
+  {
+    return 1;
+    printf("WSWD command or param has invalid size\r\n");
+  }
+  WSWD_enable_send();
+  char command_structure[SIZE_OF_WSWD_COMMAND];
+  snprintf(command_structure, SIZE_OF_WSWD_COMMAND, "%s%s%s\r\n", WSWD_ID, command, param);
+  if(HAL_UART_Transmit(&huart2, (uint8_t*)command, SIZE_OF_WSWD_COMMAND, UART_TX_TIME_OUT ) != HAL_OK)
   {
     printf("error sending to WSWD\r\n");
   }
