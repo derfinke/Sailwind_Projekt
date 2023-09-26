ec39a4f8b52b739755c1c0a452370049ec256aaa
Benedikt <benedikt-horn@gmx.de>
2023-09-26 17:11:40 +0200

    safe localization only after motor stop

--- Firmware/Sailwind/Linear_Guide/Linear_Guide.c
+++ Firmware/Sailwind/Linear_Guide/Linear_Guide.c
@@ -86,7 +86,6 @@ void Linear_Guide_update(Linear_Guide_t *lg_ptr)
 	Linear_Guide_update_movement(lg_ptr);
 	if (Localization_update_position(&lg_ptr->localization) == LOC_POSITION_UPDATED)
 	{
-		Linear_Guide_safe_Localization(lg_ptr->localization);
 		Linear_Guide_update_sail_adjustment_mode(lg_ptr);
 	}
 }
@@ -124,6 +123,7 @@ int8_t Linear_Guide_move(Linear_Guide_t *lg_ptr, Loc_movement_t movement)
 	{
 		case Loc_movement_stop:
 			Motor_stop_moving(&lg_ptr->motor); break;
+			Linear_Guide_safe_Localization(lg_ptr->localization);
 		case Loc_movement_backwards:
 			Motor_start_moving(&lg_ptr->motor, Motor_function_cw_rotation); break;
 		case Loc_movement_forward:
