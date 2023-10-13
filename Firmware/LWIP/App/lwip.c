45792fa46756a9bb5f20c3e2ed5afd5eb3b0ef98
finkbeiner <nicofinkbeiner@gmail.com>
2023-10-13 21:41:29 +0200

    save lwip.c

--- Firmware/LWIP/App/lwip.c
+++ Firmware/LWIP/App/lwip.c
@@ -152,44 +152,6 @@ void MX_LWIP_Init(void)
 /* USER CODE BEGIN 3 */
 /* USER CODE END 3 */
 }
-void MX_LWIP_Init(void)
-{
-  /* Initilialize the LwIP stack without RTOS */
-  lwip_init();
-
-  /* IP addresses initialization with DHCP (IPv4) */
-  ipaddr.addr = 0;
-  netmask.addr = 0;
-  gw.addr = 0;
-
-  /* add the network interface (IPv4/IPv6) without RTOS */
-  netif_add(&gnetif, &ipaddr, &netmask, &gw, NULL, &ethernetif_init, &ethernet_input);
-
-  /* Registers the default network interface */
-  netif_set_default(&gnetif);
-
-  if (netif_is_link_up(&gnetif))
-  {
-    /* When the netif is fully configured this function must be called */
-    netif_set_up(&gnetif);
-  }
-  else
-  {
-    /* When the netif link is down this function must be called */
-    netif_set_down(&gnetif);
-  }
-
-  /* Set the link callback function, this function is called on change of link status*/
-  netif_set_link_callback(&gnetif, ethernet_link_status_updated);
-
-  /* Create the Ethernet link handler thread */
-
-  /* Start DHCP negotiation for a network interface (IPv4) */
-  dhcp_start(&gnetif);
-
-/* USER CODE BEGIN 3 */
-/* USER CODE END 3 */
-}
 
 #ifdef USE_OBSOLETE_USER_CODE_SECTION_4
 /* Kept to help code migration. (See new 4_1, 4_2... sections) */
